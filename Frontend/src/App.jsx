04f0b4c696f437fb8cb0372be3125602d544f729
AMOGHPTL <amoghp549@gmail.com>
2026-02-11 23:46:01 +0530

    creating navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import "./App.css";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import PoolList from "./components/PoolList";
 import CreatePool from "./components/CreatePool";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Navbar/>
       <h1>Simple Decentralized Exchange</h1>
-      <ConnectButton />
+
       <CreatePool/>
       <PoolList/>
     </div>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,23 @@
+import logo from "../assets/logo.png";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 const Navbar = () => {
-    return ( <div>
-        
-    </div> );
-}
- 
+  return (
+    <div className="flex justify-between px-[30px] border-b-[2px]">
+      <div className="flex items-center gap-[20px]">
+        <img src={logo} alt="" className="w-[60px] " />
+        <p className="text-[30px] font-semibold text-gray-900">Flex-DEX</p>
+      </div>
+      <div className="flex gap-[40px] items-center">
+        <p>Swap</p>
+        <p>Explore Pools</p>
+        <p>create pool</p>
+      </div>
+      <div className="h-[60px] w-fit flex items-center">
+        <ConnectButton />
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
